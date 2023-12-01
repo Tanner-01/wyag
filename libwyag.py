6ec16c5fef7b188888bfe5697a3d99e8d4ecbe1c
Jake Tanner <tanner-01090@hotmail.com>
2023-12-01 20:25:04 +1100

    Create the wyag file and the libwyag.py file

---
+++ libwyag.py
@@ -0,0 +1,16 @@
+import argparse
+import sys  # required by argparse
+import collections
+import configparser
+from datetime import datetime
+import grp, pwd
+from fnmatch import fnmatch
+import hashlib
+from math import ceil
+import os
+import re
+import zlib
+
+
+def main():
+        print("hello world!" )
